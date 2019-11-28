d104904b005ade51073b69c3945b177101039a6b
ngounchandara <ngounchandara11@gmail.com>
2019-11-28 10:28:59 +0700

    dara02

--- app/Http/Controllers/UserViewController.php
+++ app/Http/Controllers/UserViewController.php
@@ -13,7 +13,7 @@ class UserViewController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        return view('user.viewpage');
     }
 
     /**
---
+++ resources/views/user/viewpage.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>View Page</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<style>
+    .center{
+        margin:auto;
+        padding:auto;
+    }
+</style>
+<body>
+
+
+<div class="container-fluid">
+
+
+
+
+  <div class="row">
+    <div class="col-3" style="background-color:lavender;">.col</div>
+    <div class="col-3" style="background-color:orange;">.col</div>
+    <div class="col-3" style="background-color:lavender;">.col</div>
+  </div>
+</div>
+
+</body>
+
+</html>
